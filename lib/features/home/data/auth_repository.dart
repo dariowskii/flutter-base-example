aae171c67e6fc4aa008f7371dee57c24af16332e
Dario Varriale <dario.varriale@atobit.it>
2025-03-20 18:11:30 +0100

    Add MockAuthRepository implementation for testing purposes

--- lib/features/home/data/auth_repository.dart
+++ lib/features/home/data/auth_repository.dart
@@ -11,3 +11,18 @@ abstract class AuthRepository {
   @POST("/auth/users")
   Future<HttpResponse<List<Person>>> getUsers();
 }
+
+class MockAuthRepository implements AuthRepository {
+  final Dio _dio;
+
+  MockAuthRepository(this._dio);
+
+  @override
+  Future<HttpResponse<List<Person>>> getUsers() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return HttpResponse([
+      const Person(firstName: 'John', lastName: 'Doe', age: 30),
+      const Person(firstName: 'Jane', lastName: 'Doe', age: 25),
+    ], Response(requestOptions: RequestOptions(path: '/auth/users')));
+  }
+}
